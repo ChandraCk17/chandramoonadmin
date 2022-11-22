2126b27650d67cfba79e75b7c7eb41d275befdc9
ChandraCk17 <mohitckdubey@gmail.com>
2022-11-23 05:03:52 +0530

    dummyData updated

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./topbar.css"
+import { NotificationsNone, Language, Settings } from '@material-ui/icons';
 
 export default function Topbar() {
   return (
@@ -8,8 +9,21 @@ export default function Topbar() {
             <div className="topLeft">
                 <span className="logo">chandramoonadmin</span>
             </div>
-            <div className="topRight">right</div>
+            <div className="topRight">
+              <div className="topbarIconContainer">
+                <NotificationsNone />
+                <span className='topIconBadge'>2</span>
+              </div>
+              <div className="topbarIconContainer">
+                <Language />
+                <span className='topIconBadge'>2</span>
+              </div>
+              <div className="topbarIconContainer">
+                <Settings />
+              </div>
+              <img src="https://media-exp1.licdn.com/dms/image/C4E03AQEdAMzSoTSRIg/profile-displayphoto-shrink_800_800/0/1649517237797?e=2147483647&v=beta&t=3QqPoE291MW4Fc52AlA62lpbipiF0bhLYXiWDH3wtQQ" alt="" className='topAvatar' />
+            </div>  
         </div>
     </div>
-  )
+  );
 }
