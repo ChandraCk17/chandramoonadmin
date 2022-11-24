013e6b08d3897f340575e4292e515b3dd3cf07ce
ChandraCk17 <mohitckdubey@gmail.com>
2022-11-24 23:19:42 +0530

    user page done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./app.css";
 import Home from "./pages/home/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
       <Sidebar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/users" element={<UserList />} />
+        <Route path="/user" element={<UserList />} />
+        <Route path="/user/:userId" element={<User />} />
       </Routes>
     </div>
   </Router>
---
+++ src/pages/user/User.jsx
@@ -0,0 +1,122 @@
+import {
+    CalendarToday,
+    LocationSearching,
+    MailOutline,
+    PermIdentity,
+    PhoneAndroid,
+    Publish,
+} from "@material-ui/icons"
+import { Link } from "react-router-dom";
+import "./user.css";
+
+export default function User() {
+    return (
+        <div className="user">
+            <div className="userTitleContainer">
+                <h1 className="userTitle">Edit User</h1>
+                <Link to="/newUser">
+                    <button className="userAddButton">Create</button>
+                </Link>
+            </div>
+            <div className="userContainer">
+                <div className="userShow">
+                    <div className="userShowTop">
+                        <img src="https://media-exp1.licdn.com/dms/image/C4E03AQEdAMzSoTSRIg/profile-displayphoto-shrink_800_800/0/1649517237797?e=2147483647&v=beta&t=3QqPoE291MW4Fc52AlA62lpbipiF0bhLYXiWDH3wtQQ"
+                        alt=""
+                        className="userShowImg"
+                       /> 
+                       <div className="userShowTopTitle">
+                        <span className="userShowUsername">ChandraKant Dubey</span>
+                        <span className="userShowUseTitle">Software Engineer</span>
+                    </div>
+                </div>
+                <div className="userShowBottom">
+                    <span className="userShowTitle">Account Details</span>
+                    <div className="userShowInfo">
+                        <PermIdentity className="userShowIcon" />
+                        <span className="userShowInfoTitle">Chandrack17</span>
+                    </div>
+                    <div className="userShowInfo">
+                        <CalendarToday className="userShowIcon" />
+                        <span className="userShowInfoTitle">05.08.1997</span>
+                    </div>
+                    <span className="userShowTitle">Contact Details</span>
+                    <div className="userShowInfo">
+                        <PhoneAndroid className="userShowIcon" />
+                        <span className="userShowInfoTitle">+91 123 456 67</span>
+                    </div>
+                    <div className="userShowInfo">
+                        <MailOutline className="userShowIcon" />
+                        <span className="userShowInfoTitle">Chandrack17@gmail.com</span>
+                    </div>
+                    <div className="userShowInfo">
+                        <LocationSearching className="userShowIcon" />
+                        <span className="userShowInfoTitle">Uttar Pradesh | India</span>
+                    </div>
+                  </div> 
+                </div>
+                <div className="userUpdate">
+                    <span className="userUpdateTitle">Edit</span>
+                    <form className="userUpdateForm">
+                        <div className="userUpdateLeft">
+                            <div className="userUpdateItem">
+                                <label>Username</label>
+                                <input
+                                 type="text"
+                                 placeholder="chandrack17"
+                                 className="userUpdateInput"
+                                /> 
+                            </div>
+                            <div className="userUpdateItem">
+                                <label>Full Name</label>
+                                <input 
+                                 type="text"
+                                 placeholder="Chandrakant Dubey"
+                                 className="userUpdateInput"
+                                /> 
+                            </div>
+                            <div className="userUpdateItem">
+                                <label>Email</label>
+                                <input 
+                                 type="text"
+                                 placeholder="chandrack17@gmail.com"
+                                 className="userUpdateInput"
+                                /> 
+                            </div>
+                            <div className="userUpdateItem">
+                                <label>Phone</label>
+                                <input 
+                                 type="text"
+                                 placeholder="+91 123 456 67"
+                                 className="userUpdateInput"
+                                /> 
+                            </div>
+                            <div className="userUpdateItem">
+                                <label>Address</label>
+                                <input 
+                                 type="text"
+                                 placeholder="Uttar Pradesh | India"
+                                 className="userUpdateInput"
+                                /> 
+                            </div>
+                        </div>
+                        <div className="userUpdateRight">
+                            <div className="userUpdateUpload">
+                                <img
+                                 className="userUpdateImg"
+                                 src="https://media-exp1.licdn.com/dms/image/C4E03AQEdAMzSoTSRIg/profile-displayphoto-shrink_800_800/0/1649517237797?e=2147483647&v=beta&t=3QqPoE291MW4Fc52AlA62lpbipiF0bhLYXiWDH3wtQQ"
+                                 alt=""
+                                />
+                                <label htmlFor="file">
+                                    <Publish className="userUpdateIcon" />
+                                </label> 
+                                <input type="file" id="file" style={{ display: "none" }} />
+                            </div>
+                            <button className="userUpdateButton">Update</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
